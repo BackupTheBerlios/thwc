b721f4d074dcc2599a6f985f5fe4eb71aa306f09
master_mario <master_mario>
2003-06-12 14:20:31 +0000

    an cvs angepasst

--- thwc/admin/versioninfo.php
+++ thwc/admin/versioninfo.php
@@ -1,5 +1,5 @@
 <?php
-/* $Id: versioninfo.php,v 1.1 2003/06/12 13:59:28 master_mario Exp $ */
+/* $Id: versioninfo.php,v 1.2 2003/06/12 14:20:31 master_mario Exp $ */
  include( 'adhead.inc.php' );
 
  $data['work'] = '<b>Generic information</b><br>';
@@ -15,14 +15,7 @@
  $data['work'] .= '<br>';
  $a_dir = array('../', '../inc/', './', '../mod/');
  $a_dev = array(
-     'dkreuer' => 'Daniel Kreuer',
-     'deandy' => 'Andy Karpow',
-     'slier' => 'Sascha Liehr',
-     'superhausi' => 'Stephan Hauser',
-     'pbaecher' => 'Paul Baecher',
-     'thetinysteini' => 'Sebastian Steinlechner',
-     'master_mario'  => 'Mario Pischel',
-     'nobody'  => '&nbsp;');
+     'master_mario'  => 'Mario Pischel');
 
  $data['work'] .= '<b>ThWboard file versions</b><br>';
  $data['work'] .= '<table width="100%" border="0" cellspacing="0" cellpadding="3">';
@@ -30,9 +23,7 @@
   <td id="blank"><i>Dateiname</i></td>
   <td id="blank"><i>Version</i></td>
   <td id="blank"><i>letzte &Auml;nderung</i></td>
-  <td id="blank"><i>Letzter Autor <font size="1"><br />(des Originals)</font></i></td>
-  <td id="blank"><i>Autor <font size="1"><br />(diesr Version)</font></i></td>
-  <td id="blank"><i>Ge&auml;ndert von</i></td>
+  <td id="blank"><i>Autor</i></td>
  </tr>';
 
  while( list(, $dir) = each($a_dir) )
@@ -60,15 +51,13 @@
          $datas = fread($fp, 128);
          fclose($fp);
          //^.[$()|*+?{\
-         if( ereg('/\* \$'.'Id'.': ([^[:space:]]*) ([^[:space:]]*) ([^[:space:]]*) ([^[:space:]]*) ([^[:space:]]*) ([^[:space:]]*) ([^[:space:]]*) ([^[:space:]]*) \$ \*/', $datas, $regs) )
+         if( ereg('/\* \$'.'Id'.': ([^[:space:]]*) ([^[:space:]]*) ([^[:space:]]*) ([^[:space:]]*) ([^[:space:]]*) ([^[:space:]]*) \$ \*/', $datas, $regs) )
          {
              $data['work'] .= '  <tr bgcolor="'.( $i % 2 == 0 ? '#E5E5E5' : '#F2F2F2').'">
               <td id="blank"><font size="1">'.$file.'</font></td>
               <td id="blank"><font size="1">'.$regs[2].'</font></td>
               <td id="blank"><font size="1">'.$regs[3].' '.$regs[4].'</font></td>
               <td id="blank"><font size="1">'.$a_dev[$regs[5]].'</font></td>
-              <td id="blank"><font size="1">'.$a_dev[$regs[6]].'</font></td>
-              <td id="blank"><font size="1">'.$a_dev[$regs[7]].'</font></td>
              </tr>';
              $i++;
          }
