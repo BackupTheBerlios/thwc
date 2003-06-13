4de1cc21e8ecd2b0ec217bceeea8dbd1923d57df
master_mario <master_mario>
2003-06-13 11:28:02 +0000

    SESSION_vars für openclose gesetzt GNU GPL

--- thwc/index.php
+++ thwc/index.php
@@ -1,7 +1,25 @@
 <?php
- /* $Id: index.php,v 1.2 2003/06/12 21:58:30 master_mario Exp $ */
+ /* $Id: index.php,v 1.3 2003/06/13 11:28:02 master_mario Exp $ */
+ /*
+          ThWClone - PHP/MySQL Bulletin Board System
+        ==============================================
+          (c) 2003 by
+           Mario Pischel         <mario@aqzone.de>
+
+          download the latest version:
+          https://developer.berlios.de/projects/thwc/
+
+          This  program is  free  software;  you can
+          redistribute it and/or modify it under the
+          terms of the GNU General Public License as
+          published by the Free Software Foundation;
+          either  version 2 of  the License,  or (at
+          your option) any later version.
+
+        ==============================================
+ */
  include ( 'inc/header.inc.php' );
- 
+  
  $TCatrow = Get_Template( 'templates/'.$style['styletemplate'].'/catrow.html' );
  $TBoardrow = Get_Template( 'templates/'.$style['styletemplate'].'/index_b_row.html' );
  $TIndex = Get_Template( 'templates/'.$style['styletemplate'].'/boardtable.html' ); 
@@ -16,9 +34,17 @@
      $data['boards'] = '<tr><td class="cella" colspan="5"><center>Keine Kategorien angelegt.</center></td></tr>';
  else
  {
+     if( !isset( $_SESSION['openclose'] ) ) // openclose schon erzeugt ?
+	     $_SESSION['openclose'] = '0';
      $boards = '';
      while( $a_category = db_result( $r_category ) )
 	 {
+		 $session_cat = 'c'.$a_category['category_id']; // openclose Variable
+	     if( $_SESSION['openclose'] == 0 ) // wenn nicht dann erzeugen
+		 {
+			 $_SESSION[$session_cat] = $a_category['category_is_open'];
+			 // SESSION_vars für openclose setzen
+		 }
 	     $board_count = 0; 
          $r_boards = db_query("SELECT
 	         board_id,
@@ -39,50 +65,54 @@
 		     $category = '';
 		     while( $board = db_result( $r_boards ) )
 			 {
-			     $board_count++;
+                 $P = boardPermissions ( U_GROUPIDS, $board['board_id'] );
+				 if( $P[30] == 1 )
+				 {
+			         $board_count++;
 				 
-				 $session_var = 'b'.$board['board_id'];
-				 $new_topic = 0;
-				 $gif = '';
-				 if( U_ID != 0 )
-				 {
-				     if( $board['last_post_id'] > $_SESSION[$session_var] && $_SESSION[$session_var] != 0 )
-				     {    $gif = '_new'; $new_topic = 1;    } 
-				 }
-				 $boardname = '<a href="board.php?boardid='.$board['board_id'].'"><b>'.$board['board_name'].'</b></a>';
-				 $boardname .= '<br />[smallfont]'.$board['board_under'].'[smallfontend]';
-				 $threads = $board['threads'];
-				 $posts = $board['posts'];
-				 if( P_SHOWDELETED == 1 )
-				 {
-				     $threads .= '/<font color="'.$style['col_link'].'">[smallfont]'.$board['threads_del'].'[smallfontend]</font>';
-				     $posts .= '/<font color="'.$style['col_link'].'">[smallfont]'.$board['posts_del'].'[smallfontend]</font>';
-				 }
-				 if( $board['last_act_time'] != 0 )
-				 {
-				     $last = date( "d.m.Y H:i \U\h\\r", $board['last_act_time'] );
-					 if( strlen( $board['last_act_thread'] ) > 50 )
-					     $board['last_act_thread'] = substr ( $board['last_act_thread'], 0, 46 ).'...';
-					 $last .= '<br /><a href="showthreads.php?threadid='.$board['last_thread_id'].'">'.$board['last_act_thread'].'</a>';
-					 $last .= '&nbsp;von <a href="s_profile.php?username='.$board['last_act_user'].'">'.$board['last_act_user'].'</a>';
-				 }
-				 else
-				     $last = '<center>unbekannt</center>';
+				     $session_var = 'b'.$board['board_id'];
+				     $new_topic = 0;
+			    	 $gif = '';
+		    		 if( U_ID != 0 )
+			    	 {
+				         if( $board['last_post_id'] > $_SESSION[$session_var] && $_SESSION[$session_var] != 0 )
+				         {    $gif = '_new'; $new_topic = 1;    } 
+				     }
+		    		 $boardname = '<a href="board.php?boardid='.$board['board_id'].'"><b>'.$board['board_name'].'</b></a>';
+			    	 $boardname .= '<br />[smallfont]'.$board['board_under'].'[smallfontend]';
+				     $threads = $board['threads'];
+				     $posts = $board['posts'];
+				     if( P_SHOWDELETED == 1 )
+				     {
+				         $threads .= '/<font color="'.$style['col_link'].'">[smallfont]'.$board['threads_del'].'[smallfontend]</font>';
+				         $posts .= '/<font color="'.$style['col_link'].'">[smallfont]'.$board['posts_del'].'[smallfontend]</font>';
+				     }
+				     if( $board['last_act_time'] != 0 )
+				     {
+				         $last = date( "d.m.Y H:i \U\h\\r", $board['last_act_time'] );
+					     if( strlen( $board['last_act_thread'] ) > 50 )
+					         $board['last_act_thread'] = substr ( $board['last_act_thread'], 0, 46 ).'...';
+					     $last .= '<br /><a href="showthreads.php?threadid='.$board['last_thread_id'].'">'.$board['last_act_thread'].'</a>';
+					     $last .= '&nbsp;von <a href="s_profile.php?username='.$board['last_act_user'].'">'.$board['last_act_user'].'</a>';
+				     }
+				     else
+				         $last = '<center>unbekannt</center>';
 				 
-				 $row = $TBoardrow;
-				 $row = str_replace( '[board_gif]', '<img src="templates/'.$style['styletemplate'].'/images/board'.$gif.'.gif" border="0">', $row );
-				 $row = str_replace( '[forum_name]', $boardname, $row );
-				 $row = str_replace( '[threads]', $threads, $row );
-				 $row = str_replace( '[posts]', $posts, $row );
-				 $row = str_replace( '[last]', $last, $row );
-				 $category .= $row; 
-			 }
-		 }
+				     $row = $TBoardrow;
+		    		 $row = str_replace( '[board_gif]', '<img src="templates/'.$style['styletemplate'].'/images/board'.$gif.'.gif" border="0">', $row );
+			    	 $row = str_replace( '[forum_name]', $boardname, $row );
+				     $row = str_replace( '[threads]', $threads, $row );
+     				 $row = str_replace( '[posts]', $posts, $row );
+		    		 $row = str_replace( '[last]', $last, $row );
+			    	 $category .= $row; 
+				 } // if
+			 } // while
+		 } // if
 		 $catblock = '';
 		 if( $board_count > 0 )
 		 {
 	         $catname = '<a href="category.php?catid='.$a_category['category_id'].'"><b>'.$a_category['category_name'].'</b></a>';	
-			 $catname .= '&nbsp;<font size="1">[<a href="open_cat.php?catid='.$a_category['category_id'].'&open=">open/close</a>]</font>'; 
+			 $catname .= '&nbsp;<font size="1">[<a href="open_cat.php?catid='.$a_category['category_id'].'&open='.$_SESSION[$session_cat].'">open/close</a>]</font>'; 
 		     $catimage = '';
 			 if( $new_topic == 1 ) $catimage = '_new';
 		 
@@ -90,15 +120,16 @@
 			 $row = str_replace( '[category_name]', $catname, $row );
 			 $row = str_replace( '[catimage]', '<img src="templates/'.$style['styletemplate'].'/images/board'.$catimage.'.gif" border="0" width="20" height="8">', $row );
 			 $catblock = $row;
-			 if( $a_category['category_is_open'] == 1 )
+			 if( $_SESSION[$session_cat] == 1 )
 			 {
 			     $catblock .= $category;
 			 }			
 			 $boards .= $catblock;
-		 }
-	 }
-	 
- }
+		 } // if
+	 } // while
+	 if( $_SESSION['openclose'] == 0 )
+	     $_SESSION['openclose'] = 1; // openclose Variablen als gesetzt makieren
+ } // else
  
  $data['boards'] = $boards;
  $data['user'] = U_NAME;
