0f1f07956f32a13c6caf210b7639d333454bb61a
master_mario <master_mario>
2003-07-01 21:05:54 +0000

    Initial revision

---
+++ portal/portal.php
@@ -0,0 +1,21 @@
+<?php
+ /* $Id: portal.php,v 1.1 2003/07/01 21:05:54 master_mario Exp $ */
+ /*
+          ThWClone - PHP/MySQL Bulletin Board System
+        ==============================================
+          (c) 2003 by
+           Mario Pischel         <mario@aqzone.de>
+
+          download the latest version:
+          https://developer.berlios.de/projects/thwc/
+
+          This  program is  free  software;  you can
+          redistribute it and/or modify it under the
+          terms of the GNU General Public License as
+          published by the Free Software Foundation;
+          either  version 2 of  the License,  or (at
+          your option) any later version.
+
+        ==============================================
+ */
+?>
